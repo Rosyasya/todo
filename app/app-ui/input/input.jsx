dc993050c948af759d76780af06812aa77b6e7e4
Ростиslave <opamkgue@ukr.net>
2023-06-22 16:22:05 +0300

    todo: remove button, add button

--- app/app-ui/input/input.jsx
+++ app/app-ui/input/input.jsx
@@ -2,14 +2,12 @@ import './input.scss'
 import {useState} from "react";
 import PropTypes from "prop-types";
 
-const Input = ( { placeholder, size, label, isDisabled } ) => {
-    let [value, setValue] = useState('');
-
+const Input = ( { placeholder, size, label, isDisabled, callback, inputValue } ) => {
     return (
         <div className="input__wrapper">
             <p className="input__label">{label}</p>
             <div className="input">
-                <input disabled={isDisabled} onInput={e => setValue(value = e.target.value)} placeholder={placeholder} className="input__item" style={{width: size}}/>
+                <input disabled={isDisabled} onChange={callback} value={inputValue} placeholder={placeholder} className="input__item" style={{width: size}}/>
             </div>
         </div>
     );
--- app/page.js
+++ app/page.js
@@ -5,26 +5,56 @@ import './styles/todo.scss'
 import {useState} from "react";
 
 export default function Home() {
-    const [edit, setEdit] = useState(false);
+    const [show, setShow] = useState(false);
+    const [inputAddTodoValue, setInputAddTodoValue] = useState('');
+    const [inputValue, setInputValue] = useState('');
 
-    const todoList = [
+    const [todos, setTodos] = useState([
         {
             title: 'first buy tea',
             isCompleted: false,
+            isEditing: false,
+            id: 0,
         },
         {
             title: 'second buy tea',
             isCompleted: false,
+            isEditing: true,
+            id: 1,
         },
         {
             title: 'third buy tea',
-            isCompleted: true,
+            isCompleted: false,
+            isEditing: false,
+            id: 2,
         },
         {
             title: 'fourth buy tea',
             isCompleted: false,
+            isEditing: false,
+            id: 3,
         },
-    ]
+    ]);
+
+    const inputAddTodoHandler = e => {
+        e.preventDefault();
+        setInputAddTodoValue(e.target.value);
+    }
+
+    const todoInputHandler = e => {
+        e.preventDefault();
+        setInputValue(e.target.value);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setTodos([...todos, {title: inputAddTodoValue, isCompleted: false, id: todos.length + 1}]);
+        setInputAddTodoValue('');
+    }
+
+    const removeTodo = id => {
+        setTodos(() => todos.filter(todo => todo.id !== id));
+    }
 
     return (
         <div className="todo__wrapper">
@@ -34,23 +64,25 @@ export default function Home() {
                     <Input placeholder="Search" size="150px"/>
                 </div>
                 <div className="todo__input">
-                    <Input size="550px" placeholder="Text" />
-                    <Button title="Add task" />
+                    <form action="" style={{display: "flex", justifyContent: "space-between", width: '100%'}}>
+                        <Input size="550px" placeholder="Text" callback={inputAddTodoHandler} inputValue={inputAddTodoValue} setInputValue={setInputAddTodoValue} />
+                        <Button title="Add task" callback={handleSubmit} />
+                    </form>
                 </div>
                 <div className="todo__info">
-                    {todoList.sort((a, b) => a.isCompleted - b.isCompleted).map(el => {
+                    {todos.sort((a, b) => a.isCompleted - b.isCompleted).map(todo => {
                         return (
-                            <div className={el.isCompleted ? "todo__item completed" : 'todo__item'}>
-                                <input type="checkbox" checked={el.isCompleted} />
-                                <div className="todo__item-title" style={{display: edit ? 'none' : 'block'}}>
-                                    <p>{el.title}</p>
+                            <div className={todo.isCompleted ? "todo__item completed" : 'todo__item'}>
+                                <input type="checkbox" />
+                                <div className="todo__item-title" style={{display: todo.isEditing ? 'none' : 'block'}}>
+                                    <p>{todo.title}</p>
                                 </div>
-                                <div style={{display: edit ? 'block' : 'none', flex: 1}}>
-                                    <Input size="100%"/>
+                                <div style={{display: todo.isEditing ? 'block' : 'none', flex: 1}}>
+                                    <Input size="100%" isDisabled={show} placeholder={todo.title} callback={todoInputHandler} inputValue={inputValue} setInputValue={setInputValue} show={show} />
                                 </div>
                                 <div className="circle __star"></div>
-                                <div className="circle __edit"></div>g
-                                <div className="circle __cross"></div>
+                                <div className="circle __edit" onClick={() => setShow(!show)}></div>
+                                <div className="circle __cross" onClick={() => removeTodo(todo.id)}></div>
                             </div>
                         )
                     })}
