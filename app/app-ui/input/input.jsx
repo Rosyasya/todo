122bf3e5a491c394f3d4c0f84681d8eec601f44a
Ростиslave <opamkgue@ukr.net>
2023-06-25 13:30:43 +0300

    todo: todo-form fixed

--- app/app-ui/input/input.jsx
+++ app/app-ui/input/input.jsx
@@ -1,18 +1,12 @@
 import './input.scss'
 import PropTypes from "prop-types";
 
-const Input = ( { placeholder, size, label, isDisabled, callback, show } ) => {
-    const inputClear = e => {
-        if (e.key === 'Enter') {
-            e.target.value = '';
-        }
-    }
-
+const Input = ( { placeholder, size, label, value, isDisabled, callback, show } ) => {
     return (
         <div className="input__wrapper">
             <p className="input__label">{label}</p>
             <div className="input">
-                <input disabled={isDisabled ?? show} onKeyDown={inputClear} onChange={callback} placeholder={placeholder} className="input__item" style={{width: size}}/>
+                <input disabled={isDisabled ?? show} value={value} onChange={callback} placeholder={placeholder} className="input__item" style={{width: size}}/>
             </div>
         </div>
     );
@@ -22,6 +16,7 @@ Input.propTypes = {
     placeholder: PropTypes.string,
     size: PropTypes.string,
     label: PropTypes.string,
+    value: PropTypes.string,
     isDisabled: PropTypes.bool,
     callback: PropTypes.func,
     show: PropTypes.bool,
--- app/component/todo-form/todo-form.jsx
+++ app/component/todo-form/todo-form.jsx
@@ -5,22 +5,25 @@ import {useState} from "react";
 import PropTypes from "prop-types";
 
 const TodoForm = ( { onCreate } ) => {
+    const [value, setValue] = useState('');
     const [todo, setTodo] = useState({
         title: '',
         isCompleted: false,
     });
      const createTodo = e => {
+        setValue(e.target.value);
         setTodo({title: e.target.value, isCompleted: false});
     }
     const onSubmit = e => {
         e.preventDefault();
         onCreate(todo);
+        setValue('');
     }
 
     return (
         <div className="todo__input">
             <form action="" style={{display: "flex", justifyContent: "space-between", width: '100%'}}>
-                <Input size="550px" placeholder="Text" callback={createTodo} />
+                <Input size="550px" placeholder="Text" callback={createTodo} value={value} />
                 <Button title="Add task" callback={onSubmit} />
             </form>
         </div>
